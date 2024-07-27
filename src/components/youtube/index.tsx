67be54a372d36755d387f01fbcc3c2e6877b4be7
“brunop.nubiral“ <bruno.pedraza@nubiral.com>
2024-07-27 15:06:45 -0300

    minifix

---
+++ src/components/youtube/index.tsx
@@ -0,0 +1,9 @@
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+
+export const TrailerPlayer = () => {
+  return (
+<MediaPlayer playsInline title="Sprite Fight" src="youtube/_cMxraX_5RE">
+  <MediaProvider />
+</MediaPlayer>
+  )
+}
